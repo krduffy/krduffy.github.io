f94ae6068e9ad7ae1f0c76989fb7de1806edc094
yincade <101445104+krduffy@users.noreply.github.com>
2025-01-09 17:21:40 -0500

    changes to positions & animations on main page

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-console.log("linked");
 
-const githubButton = document.getElementById("github-button");
-const linkedInButton = document.getElementById("linkedin-button");
-const mailToButton = document.getElementById("mailto-button");
-
-githubButton.addEventListener("click", () => {
-  window.open("https://github.com/krduffy");
-});
-
-linkedInButton.addEventListener("click", () => {
-  window.open("https://www.linkedin.com/in/kieran-duffy-b9a445228/");
-});
-
-mailToButton.addEventListener("click", () => {
-  const myEmail = "iwillfillinlater";
-  window.open(`mailto:${myEmail}`);
-});
