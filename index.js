a2b012705826a5a4cc1ae1bf969190c6dbe93298
yincade <101445104+krduffy@users.noreply.github.com>
2025-01-10 13:30:29 -0500

    made animation of characters take time to start again

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const hangulCharacter = document.getElementById("hangul-character");
 const hanjaCharacter = document.getElementById("hanja-character");
 const allCharacters = [romanCharacter, hangulCharacter, hanjaCharacter];
 
+const timers = [];
+
 function pauseAll() {
   allCharacters.forEach((character) => {
     character.style.animationPlayState = "paused";
   });
+  timers.forEach((timer) => clearTimeout(timer));
 }
 
 function resumeAll() {
   allCharacters.forEach((character) => {
-    character.style.animationPlayState = "running";
+    timers.push(
+      setTimeout(() => {
+        character.style.animationPlayState = "running";
+      }, 333)
+    );
   });
 }
 
