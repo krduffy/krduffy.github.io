f34aec81c7fc871d12aed56eddab08b5b670586d
yincade <101445104+krduffy@users.noreply.github.com>
2025-01-10 13:14:09 -0500

    circling scripts from dictionary added to index

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const romanCharacter = document.getElementById("roman-character");
+const hangulCharacter = document.getElementById("hangul-character");
+const hanjaCharacter = document.getElementById("hanja-character");
+const allCharacters = [romanCharacter, hangulCharacter, hanjaCharacter];
 
+function pauseAll() {
+  allCharacters.forEach((character) => {
+    character.style.animationPlayState = "paused";
+  });
+}
+
+function resumeAll() {
+  allCharacters.forEach((character) => {
+    character.style.animationPlayState = "running";
+  });
+}
+
+allCharacters.forEach((character) => {
+  character.addEventListener("mouseover", () => pauseAll());
+  character.addEventListener("mouseleave", () => resumeAll());
+});
